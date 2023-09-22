9a88475d7f13e592d573144108300d31adce7e4b
Eriksson Hou <hosealle@outlook.com>
2023-09-22 20:51:02 +0800

    前后端合并打包后，简单get请求成功，关闭程序后后端服务还存在

--- src/main.js
+++ src/main.js
@@ -1,53 +1,91 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
+const {spawn} = require('child_process');
+const log = require('electron-log');
+
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+// 后端主进程
+let server
+// 后端jar包名称
+const serverName = 'Application.jar'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  app.quit();
+    app.quit();
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    });
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+    // and load the index.html of the app.
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    } else {
+        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    }
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+    if (startServer()) {
+        createWindow()
+    } else {
+        app.quit()
+    }
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        const kill = require('tree-kill');
+        kill(server.pid, 'SIGTERM', function (err) {
+            log.info(err)
+            log.info('Server killed')
+        })
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+// 自定义函数
+
+const startServer = () => {
+    let serverPath = `${path.join(app.getAppPath(), 'src', 'server', serverName)}`
+    log.info(`Server path is ${serverPath}`)
+    log.info(`Launching server with jar ${serverName}`)
+    server = spawn('java', ['-jar', serverPath], {shell: true})
+    if (server.pid) {
+        log.info(`Server launched with pid ${server.pid}`)
+        return true
+    } else {
+        log.error('Server failed to launch')
+        app.quit()
+        return false
+    }
+}
